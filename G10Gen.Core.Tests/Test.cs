f16f6277b758346915444f28d3b751f95afa80e9
MoritzOtto <moritz-otto@gmx.de>
2015-12-07 20:08:17 +0100

    InitialCommit

---
+++ G10Gen.Core.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace G10Gen.Core.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+    }
+}
